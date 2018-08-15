26924eeb15f196a8178bf9086997172734a32a80
Your Name <you@example.com>
2018-08-15 12:17:58 +0900

    m

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
     Paperclip::Attachment.default_options[:storage] = :gcs
     Paperclip::Attachment.default_options[:gcs_bucket] = "solidus"
     Paperclip::Attachment.default_options[:url] = ":gcs_path_url"
-    Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id/:style/:filename"
+    Paperclip::Attachment.default_options[:path] = "images/:id/:style/:filename"
     Paperclip::Attachment.default_options[:gcs_credentials] = {
         project: "root-talon-161808",#ENV["GCS_PROJECT"],
         keyfile: "/var/www/gcs_keyfile",#ENV["GCS_KEYFILE"],
