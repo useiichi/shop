1124a6ee55bf9b1123d129a55bca12fbb3761b79
Your Name <you@example.com>
2018-08-18 22:33:09 +0900

    m

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -8,6 +8,9 @@ if Rails.env.production?
   Paperclip::Attachment.default_options[:url] = ":gcs_path_url"
   Paperclip::Attachment.default_options[:path] = "products/:id/:style/:basename.:extension"#"products/:id/:style/:filename"
   Paperclip::Attachment.default_options[:gcs_host_name] = "hanbai.iseisaku.com"
+  # memo https://storage.googleapis.com/solidus/products/11/product/solidus_logo.png?1534598968
+  #      ↓
+  #      https://hanbai.iseisaku.com/solidus/products/11/product/solidus_logo.png?1534598968
   Paperclip::Attachment.default_options[:gcs_credentials] = {
       project: "root-talon-161808",#ENV["GCS_PROJECT"],
       keyfile: "/var/www/gcs_keyfile",#ENV["GCS_KEYFILE"],
