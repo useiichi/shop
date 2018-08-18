8a08456e7b762ca0d3c4149e6325028c9f19ab8b
Your Name <you@example.com>
2018-08-18 21:52:43 +0900

    m

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,12 +3,15 @@ if Rails.env.production?
   #  Paperclip::Attachment.default_options[:url] = ':gcs_domain_url'
   #  Paperclip::Attachment.default_options[:fog_directory] = "solidus"#ENV['GCS_BUCKET_NAME']
   
-    Paperclip::Attachment.default_options[:storage] = :gcs
-    Paperclip::Attachment.default_options[:gcs_bucket] = "solidus"
-    Paperclip::Attachment.default_options[:url] = ":gcs_path_url"
-    Paperclip::Attachment.default_options[:path] = "images/:id/:style/:filename"
-    Paperclip::Attachment.default_options[:gcs_credentials] = {
-        project: "root-talon-161808",#ENV["GCS_PROJECT"],
-        keyfile: "/var/www/gcs_keyfile",#ENV["GCS_KEYFILE"],
-    }
-  end
+  Paperclip::Attachment.default_options[:storage] = :gcs
+  Paperclip::Attachment.default_options[:gcs_bucket] = "solidus"
+  Paperclip::Attachment.default_options[:url] = ":gcs_path_url"
+  Paperclip::Attachment.default_options[:path] = "images/:id/:style/:filename"
+  Paperclip::Attachment.default_options[:gcs_credentials] = {
+      project: "root-talon-161808",#ENV["GCS_PROJECT"],
+      keyfile: "/var/www/gcs_keyfile",#ENV["GCS_KEYFILE"],
+  }
+
+  Spree::Image.attachment_definitions[:attachment].delete(:url)
+  Spree::Image.attachment_definitions[:attachment].delete(:path)
+end
