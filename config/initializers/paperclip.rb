7eec2b499b1de30ba977e25eff7f07efe997ca42
Your Name <you@example.com>
2018-08-18 22:13:24 +0900

    m

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,13 +5,14 @@ if Rails.env.production?
   
   Paperclip::Attachment.default_options[:storage] = :gcs
   Paperclip::Attachment.default_options[:gcs_bucket] = "solidus"
-  Paperclip::Attachment.default_options[:url] = "https://hanbai.iseisaku.com/"#":gcs_path_url"
-  Paperclip::Attachment.default_options[:path] = "/products/:id/:style/:basename.:extension"#"products/:id/:style/:filename"
+  Paperclip::Attachment.default_options[:url] = ":gcs_path_url"
+  Paperclip::Attachment.default_options[:path] = "products/:id/:style/:basename.:extension"#"products/:id/:style/:filename"
   Paperclip::Attachment.default_options[:gcs_credentials] = {
       project: "root-talon-161808",#ENV["GCS_PROJECT"],
       keyfile: "/var/www/gcs_keyfile",#ENV["GCS_KEYFILE"],
   }
 
-  Spree::Image.attachment_definitions[:attachment].delete(:url)
+  #Spree::Image.attachment_definitions[:attachment].delete(:url)
+  Spree::Image.attachment_definitions[:attachment][:url] = 'https://hanbai.iseisaku.com/'
   Spree::Image.attachment_definitions[:attachment].delete(:path)
 end
